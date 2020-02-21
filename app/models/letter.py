7444390ff30ecaaaed32982fe1719452d20b4b37
mde-pach <12986913+mde-pach@users.noreply.github.com>
2020-02-21 15:56:25 +0100

    make letter model serializable

--- app/models/letter.py
+++ app/models/letter.py
@@ -7,3 +7,10 @@ class Letter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tracking_number = db.Column(db.String(256))
     status = db.Column(db.String(191))
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'tracking_number': self.tracking_number,
+            'status': self.status
+        }
